ab98d59e54148a3493bf3e5835a7411d172cfe24
THug24 <devilskipper@gmail.com>
2024-09-20 12:51:34 +0700

    Setup backend

---
+++ SS_BackEnd/src/main/java/SS_BackEnd/SsBackEndApplication.java
@@ -0,0 +1,13 @@
+package SS_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsBackEndApplication.class, args);
+	}
+
+}
---
+++ SS_BackEnd/src/test/java/SS_BackEnd/SsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package SS_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
